190d2880f0a75964cb5f62f8a0cb306adcd92018
thiago <thiagitosouza@hotmail.com>
2014-12-04 00:25:36 -0200

    Add basics packages

--- cookbooks/mezuro/recipes/default.rb
+++ cookbooks/mezuro/recipes/default.rb
@@ -1,8 +1,33 @@
-#
-# Cookbook Name:: mezuro
-# Recipe:: default
-#
-# Copyright 2014, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
+# License: GPL v3
+# Author: @thiagovsk (2014)
+# Mezuro installation recipes.
+
+# git package
+package "git" do
+	action :install
+end
+
+# tmux is a terminal multiplexer
+package "tmux" do
+	action :install
+end
+
+# Text editor
+package "vim" do
+	action :install
+end
+
+#curl package
+package "curl" do
+	action :install
+end
+
+#build-essential package
+package "build-essential" do
+	action :install
+end
+
+#postgresql-server-dev-9.3 package
+package "postgresql-server-dev-9.3" do
+	action :install
+end
